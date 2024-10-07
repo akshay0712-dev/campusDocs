923c61acc22a44be907d67e68cf0ecc321f161b8
AkshayGit <akshayrishu4@gmail.com>
2024-10-07 10:36:43 +0530

    add files

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,23 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import map from "./map";
 const img = ["0001", "0002", "0003", "0004", "0005"];
 const Calendar = () => {
+  const [InductionHeight, setInductionHeight] = useState(0);
+  const toggleHeight = (minH, maxH) => {};
+
   return (
     <>
       {/* <p className="text-center my-6 text-xl md:text-4xl text-[#1b5912]">
         Hang tight! This feature is coming soon
       </p> */}
-
-      <div className="h-[80vh] w-[90vw] md:w-[50vw] custom-scrollbar my-6 rounded-xl mx-auto overflow-scroll ">
-        {img.map((img, i) => (
-          <div key={img} className="overflow-hidden ">
-            <img
-              src={`./docs/InductionProgam2024/InductionProgam2024_page-${img}.jpg`}
-              alt=""
-              className="md:w-[50vw] w-[90vw] "
-            />
+      <div className="mx-auto w-[90vw] flex my-6 gap-3 rounded-t-xl">
+        <div className="mx-auto w-[90vw] md:w-[50vw] bg-white rounded-t-xl">
+          <div
+            className="text-center text-2xl pt-3 my-3 border-b-2"
+            onClick={() => toggleHeight(80, 0)}
+          >
+            Induction Programme Schedule {InductionHeight}
           </div>
-        ))}
+
+          <div
+            className={`h-[${InductionHeight}vh] custom-scrollbar rounded-xl ${
+              InductionHeight == 0
+                ? "overflow-hidden bg-red-700"
+                : "bg-green-700"
+            } `}
+          >
+            {img.map((img, i) => (
+              <div key={img} className=" ">
+                <img
+                  src={`./docs/InductionProgam2024/InductionProgam2024_page-${img}.jpg`}
+                  alt=""
+                  className="md:w-[50vw] w-[90vw] "
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-[45vw] ">
+          <div className="text-center text-2xl pt-3 border-b-2 bg-white rounded-t-xl">
+            Computer Science Time Table
+          </div>
+          {/* <div className="bg-white text-center w-[45vw]  ">Computer Science Time Table</div> */}
+          <img
+            src="images/RoutineCS.jpg"
+            alt="Routine_CS"
+            className="rounded-b-xl mb-2"
+          />
+          <div className="text-center text-2xl pt-3 border-b-2 bg-white rounded-t-xl">
+            Computer Science (AI & ML) Time Table
+          </div>
+          <img
+            src="images/RoutineAI_ML.jpg"
+            alt="Routine_CS"
+            className="rounded-b-xl mb-3"
+          />
+          <div className="text-center text-2xl pt-3 border-b-2 bg-white rounded-t-xl">
+            ECE Time Table
+          </div>
+          <img
+            src="images/RoutineECE.jpg"
+            alt="Routine_ECE"
+            className="rounded-b-xl mb-3 "
+          />
+          <div className="text-center text-2xl pt-3 border-b-2 bg-white rounded-t-xl">
+            Civil Time Table
+          </div>
+          <img
+            src="images/CivilRoutin.jpg"
+            alt="Routine_CS"
+            className="rounded-b-xl mb-2"
+          />
+        </div>
       </div>
     </>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
 
+const today = new Date();
+const dayOfWeek = today.toLocaleString('default', { weekday: 'long' });
+
+
+const messMenu = [
+  {
+    day: "Monday",
+    breakfast: "Puri, Aloo Matar Subji",
+    lunch: "",
+    dinner: ""
+  },
+  {
+    day: "Tuesday",
+    breakfast: "Chapati, Aloo Chana Subji ",
+    lunch: "",
+    dinner: ""
+  },
+  {
+    day: "Wednesday",
+    breakfast: "",
+    lunch: "Fish Curry/ Matar Paneer, Rice",
+    dinner: ""
+  },
+  {
+    day: "Thursday",
+    breakfast: "",
+    lunch: "",
+    dinner: ""
+  },
+  {
+    day: "Friday",
+    breakfast: "Chole Bhature",
+    lunch: "",
+    dinner: "Egg Curry/ Matar Paneer, Rice, Chapati"
+  },
+  {
+    day: "Saturday",
+    breakfast: "Aloo Paratha, Tomato Chutney",
+    lunch: "Dal, Rice , Aloo Bhindi Bhujia, Salade",
+    dinner: "Veg Biryani, Cucumber Raita"
+  },
+  {
+    day: "Sunday",
+    breakfast: "Puri, Chana Subji",
+    lunch: "Dal, Rice , Aloo Bhujia, Aloo Chokha, Onion Pakora",
+    dinner: "Chicken Curry/Matar Mushroom Subji, Rice, Chapati, Salade"
+  }
+];
+
+
+
+
 const Menu = () => {
+  console.log(dayOfWeek);
+
   return (
+    <>
     <h1>Menu</h1>
+    <div className="">
+      <div className="">
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+      <div className=""></div>
+    </div>
+    </>
     
   )
 }
