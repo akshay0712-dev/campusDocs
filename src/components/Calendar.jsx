d092f7f88d5898ec302164fa08c6807690464924
AkshayGit <akshayrishu4@gmail.com>
2024-10-09 10:27:33 +0530

    add more routine and make ui responsive

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,42 +2,36 @@ import React, { useState } from "react";
 import map from "./map";
 const img = ["0001", "0002", "0003", "0004", "0005"];
 const Calendar = () => {
-  const [InductionHeight, setInductionHeight] = useState(0);
-  const toggleHeight = (minH, maxH) => {};
 
   return (
     <>
       {/* <p className="text-center my-6 text-xl md:text-4xl text-[#1b5912]">
         Hang tight! This feature is coming soon
       </p> */}
-      <div className="mx-auto w-[90vw] flex my-6 gap-3 rounded-t-xl">
+      <div className="mx-auto w-[90vw] flex flex-col-reverse md:flex-row my-6 gap-3 rounded-t-xl">
         <div className="mx-auto w-[90vw] md:w-[50vw] bg-white rounded-t-xl">
           <div
             className="text-center text-2xl pt-3 my-3 border-b-2"
-            onClick={() => toggleHeight(80, 0)}
+           
           >
-            Induction Programme Schedule {InductionHeight}
+            Induction Programme Schedule 
           </div>
 
           <div
-            className={`h-[${InductionHeight}vh] custom-scrollbar rounded-xl ${
-              InductionHeight == 0
-                ? "overflow-hidden bg-red-700"
-                : "bg-green-700"
-            } `}
+            className={`custom-scrollbar rounded-xl  `}
           >
             {img.map((img, i) => (
               <div key={img} className=" ">
                 <img
                   src={`./docs/InductionProgam2024/InductionProgam2024_page-${img}.jpg`}
                   alt=""
-                  className="md:w-[50vw] w-[90vw] "
+                  className="md:w-[50vw] w-[90vw]  6"
                 />
               </div>
             ))}
           </div>
         </div>
-        <div className="w-[45vw] ">
+        <div className="w-[90vw] md:w-[45vw] ">
           <div className="text-center text-2xl pt-3 border-b-2 bg-white rounded-t-xl">
             Computer Science Time Table
           </div>
